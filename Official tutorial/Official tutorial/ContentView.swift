8bddd95826d76229261f1b1f1e3ac84e6b610f65
zeroskylian <zeroskylian@gmail.com>
2024-09-20 09:28:47 +0800

    Official tutorial chapter 1

--- Official tutorial/Official tutorial/ContentView.swift
+++ Official tutorial/Official tutorial/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import ComposableArchitecture
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
--- Official tutorial/Official tutorial/Official_tutorialApp.swift
+++ Official tutorial/Official tutorial/Official_tutorialApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct Official_tutorialApp: App {
+    
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: Self.store)
         }
     }
 }
