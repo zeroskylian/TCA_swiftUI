215ede6dba9e3fc4c961920de704912ed54ee7f9
zeroskylian <zeroskylian@gmail.com>
2024-09-24 14:07:30 +0800

    Official tutorial chapter 2

--- Official tutorial/Official tutorial/Feature/CounterFeature.swift
+++ Official tutorial/Official tutorial/Feature/CounterFeature.swift
@@ -14,22 +14,61 @@ struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
+        var isTimerRunning = false
     }
     
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
+        case toggleTimerButtonTapped
+        case timerTick
     }
+    
+    enum CancelID { case timer }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
-                
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
+                return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+            case .factResponse(let fact):
+                state.fact = fact
+                state.isLoading = false
+                return .none
+            case .toggleTimerButtonTapped:
+                if !state.isTimerRunning {
+                    state.isTimerRunning.toggle()
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }.cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
                 return .none
             }
         }
@@ -64,14 +103,38 @@ struct CounterView: View {
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
             }
+            Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+                store.send(.toggleTimerButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            Button("Fact") {
+                store.send(.factButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            if store.isLoading {
+                ProgressView()
+            } else if let fact = store.fact {
+                Text(fact)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
         }
     }
 }
 
 #Preview {
-  CounterView(
-    store: Store(initialState: CounterFeature.State()) {
-      CounterFeature()
-    }
-  )
+    CounterView(
+        store: Store(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+    )
 }
