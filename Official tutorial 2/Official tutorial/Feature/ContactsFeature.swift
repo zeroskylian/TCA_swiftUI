d83f943f9684e08a6e5096366eb2d6f07ad76b13
zeroskylian <zeroskylian@gmail.com>
2024-10-16 19:45:05 +0800

    feat: Multiple presentation destinations section 2

--- Official tutorial 2/Official tutorial/Feature/ContactsFeature.swift
+++ Official tutorial 2/Official tutorial/Feature/ContactsFeature.swift
@@ -19,58 +19,66 @@ struct ContactsFeature {
     @ObservableState
     struct State: Equatable {
         
-        @Presents var addContact: AddContactFeature.State?
-        
-        @Presents var alert: AlertState<Action.Alert>?
-        
         var contacts: IdentifiedArrayOf<Contact> = []
+        
+        @Presents var destination: Destination.State?
     }
     
     enum Action {
         
         case addButtonTapped
         
-        case addContact(PresentationAction<AddContactFeature.Action>)
-        
-        case alert(PresentationAction<Alert>)
-        
         case deleteButtonTapped(id: Contact.ID)
+        
+        case destination(PresentationAction<Destination.Action>)
         
         enum Alert: Equatable {
             case confirmDeletion(id: Contact.ID)
         }
     }
     
-    var body: some ReducerOf<Self> {
+    var body: some ReducerOf<ContactsFeature> {
         Reduce { state, action in
             switch action {
             case .addButtonTapped:
-                state.addContact = AddContactFeature.State(
-                    contact: Contact(id: UUID(), name: "")
+                state.destination = .addContact(
+                    AddContactFeature.State(
+                        contact: Contact(id: UUID(), name: "")
+                    )
                 )
                 return .none
-            case .addContact(.presented(.delegate(.saveContact(let contact)))):
+            case .deleteButtonTapped(id: let id):
+                state.destination = .alert(
+                    AlertState {
+                        TextState("Are you sure?")
+                    } actions: {
+                        ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                            TextState("Delete")
+                        }
+                    }
+                )
+                return .none
+            case .destination(.presented(.addContact(.delegate(.saveContact(let contact))))):
                 state.contacts.append(contact)
                 return .none
-            case .addContact:
-                return .none
-            case let .alert(.presented(.confirmDeletion(id: id))):
+            case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
                 state.contacts.remove(id: id)
                 return .none
-            case .alert:
-                return .none
-            case .deleteButtonTapped(id: let id):
-                state.alert = AlertState {
-                    TextState("Are you sure?")
-                } actions: {
-                    ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
-                        TextState("Delete")
-                    }
-                }
+            case .destination:
                 return .none
             }
-        }.ifLet(\.$addContact, action: \.addContact) {
-            AddContactFeature()
-        }.ifLet(\.$alert, action: \.alert)
+        }.ifLet(\.$destination, action: \.destination)
     }
 }
+
+
+extension ContactsFeature {
+    /// 拆分的原因是你的目标是在同一时段只能有一个弹窗, 但是之前的场景可能在同时弹出两个
+    @Reducer
+    enum Destination {
+        case addContact(AddContactFeature)
+        case alert(AlertState<ContactsFeature.Action.Alert>)
+    }
+}
+
+extension ContactsFeature.Destination.State: Equatable {}
