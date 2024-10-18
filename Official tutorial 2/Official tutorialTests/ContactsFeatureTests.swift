86dd00b50f3fd40d4e7a171ad8ccc23e2f2e8c2e
zeroskylian <zeroskylian@gmail.com>
2024-10-18 09:39:09 +0800

    feat: Test presentation destinations section 2

--- Official tutorial 2/Official tutorialTests/ContactsFeatureTests.swift
+++ Official tutorial 2/Official tutorialTests/ContactsFeatureTests.swift
@@ -52,4 +52,28 @@ struct ContactsFeatureTests {
             $0.destination = nil
         }
     }
+    
+    /// 非详尽模式
+    /// addFlow 测试非常详尽且强大, 但是太繁琐了, 我们必须断言子特征中的一切如何演变 ，并且必须断言商店如何接收每个效果操作
+    /// 有时，以不太详尽的方式编写测试可能很有用，特别是在测试许多功能的集成时，例如执行导航的功能的情况。
+    @Test
+    func addFlowNonExhaustive() async {
+        let store = TestStore(initialState: ContactsFeature.State()) {
+            ContactsFeature()
+        } withDependencies: {
+            $0.uuid = .incrementing
+        }
+        
+        store.exhaustivity = .off
+        await store.send(.addButtonTapped)
+        await store.send(\.destination.addContact.setName, "Blob Jr.")
+        await store.send(\.destination.addContact.saveButtonTapped)
+        await store.skipReceivedActions()
+        store.assert {
+            $0.contacts = [
+                Contact(id: UUID(0), name: "Blob Jr.")
+            ]
+            $0.destination = nil
+        }
+    }
 }
